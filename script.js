197e89165271bf9ea09b5dcf967229419c8f7af6
leonardsim <SIMLE1@student.op.ac.nz>
2017-04-05 23:19:28 +1200

    Added features that could extract data from XML

--- script.js
+++ script.js
@@ -12,20 +12,37 @@ if (window.XMLHttpRequest) {
 // 2nd parameter is the location of data file
 // 3rd parameter is a boolean that specifies whether we want the request to be asynchronous or not
 // false = synchronous request (dont add anything for asynchronous)
-request.open('GET', 'data.txt');
+request.open('GET', 'data.xml');
 
 request.onreadystatechange = function() {
 	if (request.status === 200 && request.readyState === 4) {
 		/*You can also target specific unordered list tag by getting the index of the getElementsByTagName
 		You can target specific index element of the list tag
+
 		var modify = document.getElementsByTagName('ul')[1].getElementsByTagName('li');
-		modify[2].innerHTML = request.responseText;*/
+
+		modify[2].innerHTML = request.responseText;
 
 		var modify = document.getElementsByTagName('li');
 
 		for (var i = 0; i < modify.length; i++) {
 			modify[i].innerHTML = request.responseText;
+		}*/
+
+		// FOR XML
+		console.log(request.responseXML.getElementsByTagName('name')[1].firstChild.nodeValue);
+
+		var items = request.responseXML.getElementsByTagName('name');
+
+		var output = '<ul>';
+
+		for (var i = 0; i < items.length; i++) {
+			output += '<li>' + items[i].firstChild.nodeValue + '</li>';
 		}
+
+		output += '</ul>';
+
+		document.getElementById('update').innerHTML = output;
 	}
 }
 
